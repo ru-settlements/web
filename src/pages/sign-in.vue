2aadec55e090177a8500ab9ee60156b4cef2139b
martiiian <jkramorevx@gmail.com>
2023-06-10 19:12:29 +0300

    Init sign-in makeup

--- src/pages/sign-in.vue
+++ src/pages/sign-in.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>signin</div>
+  <form action="" class="sign-in">
+    <UIInput label="Логин / Эл. почта" />
+  </form>
 </template>
 
 <script setup lang="ts">
+import {UIInput} from "@ru-settlements/ui-components";
+
 definePageMeta({
   layout: 'guest'
 })
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.sign-in {
+  padding-inline: 10px;
+  padding-block: 10px;
+}
+</style>
