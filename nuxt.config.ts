791afdbf48846bd2ac2316655ebb42ff84c88659
martiiian <jkramorevx@gmail.com>
2023-06-10 18:35:58 +0300

    Fix pages path error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src/',
-  dir: {
-    pages: 'src/pages/'
-  },
   runtimeConfig: {
     public: {
       baseUrl: 'http://localhost:3000/'
