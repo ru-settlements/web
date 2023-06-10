d9efe6acebd30bac99630caf32fbc539469b7297
martiiian <jkramorevx@gmail.com>
2023-06-10 18:37:00 +0300

    Add example component from ui-components

--- src/app.vue
+++ src/app.vue
@@ -5,4 +5,23 @@
 </template>
 
 <script lang="ts" setup>
+import '@ru-settlements/ui-components/dist/lib.css'
+import '~/assets/styles/main.scss'
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
 </script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>
-    <h1>тут хедер будет</h1>
-    <slot />
-  </div>
+  <NavigationMenu :items="items" />
+  <slot/>
 </template>
 
 <script lang="ts" setup>
+import { NavigationMenu } from "@ru-settlements/ui-components";
+import { ref } from "@vue/reactivity";
 
+const items = ref([
+  {
+    link: '#',
+    name: 'Новости',
+  },
+  {
+    link: '#',
+    name: 'Информация',
+  },
+  {
+    link: '#',
+    name: 'Объявления',
+  },
+  {
+    link: '#',
+    name: 'История',
+  },
+  {
+    link: '#',
+    name: 'Туристу',
+  }
+])
 </script>
