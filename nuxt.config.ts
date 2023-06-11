2cdadab2a441e96c25172ec0b6d7a494fb5d10b1
martiiian <jkramorevx@gmail.com>
2023-06-11 18:46:48 +0300

    Refactor global styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src/',
+  css: [
+      '@ru-settlements/ui-components/dist/lib.css',
+      '~/assets/styles/main.scss'
+  ],
   runtimeConfig: {
     public: {
       baseUrl: 'http://localhost:3000/'
     }
-  }
+  },
 })
