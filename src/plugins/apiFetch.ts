6a90da2f620a36bafc1927c77c1a6609d5994c84
martiiian <jkramorevx@gmail.com>
2023-06-11 18:47:58 +0300

    Remove unused apiFetch plugin

--- src/plugins/apiFetch.ts
+++
@@ -1,8 +0,0 @@
-import { ofetch } from 'ofetch';
-import { defineNuxtPlugin } from '#app';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  globalThis.$apiFetch = ofetch.create({
-    baseURL: nuxtApp.$config.public.baseUrl
-  })
-});
